292ca5f721bb09fc0dc5794b2f47805b5281f3dc
Shruthi <shruthimarri97@gmail.com>
2021-01-13 12:06:03 +0530

    Added: condition of total working hours and hours

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,28 +1,35 @@
 #!/bin/bash -x
 
-empRatePerHr=20
-isFullTime=1
-isPartTime=2
-totalSalary=0
-numWorkingDays=30
+#constant
 
-for (( day=1; day<=$numWorkingDays; day++))
+EMP_RATE_PER_HR=20
+IS_FULL_TIME=1
+IS_PART_TIME=2
+NUM_OF_WORKING_DAYS=20
+MAX_HRS_IN_MONTH=100
+
+#variable
+totalEmpHrs=0
+totalWorkingDays=0
+
+while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH && $totalworkingDays -lt $NUM_OF_WORKING_DAYS ]]
 do
-	empCheck=$((RANDOM%3))
+	((totalWorkingDays++))
+	empCheck=$(( RANDOM%3 ))
 	case $empCheck in
-		$isFullTime)
+		$IS_FULL_TIME)
 			empHrs=8
 			;;
-		$isPartTime)
+		$IS_PART_TIME)
 			empHrs=4
 			;;
 		*)
 			empHrs=0
 			;;
 	esac
-salary=$(($empHrs*$empRatePerHr))
-totalSalary=$(($totalSalary+$salary))
+
+totalEmpHrs=$(($totalEmpHrs+$empHrs))
+
 done
 
-
-
+totalSalary=$(($totalEmpHrs*$EMP_RATE_PER_HR))
