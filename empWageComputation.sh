bb12019106f3af6a35de26d2e0ce85bf2b1946a2
Shruthi <shruthimarri97@gmail.com>
2021-01-13 12:44:42 +0530

    Added: storing daily wages and total wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,39 +1,45 @@
-#!/bin/bash -x
+#!/bin/bash -x 
 
-#constants
+#consants
 EMP_RATE_PER_HR=20
 IS_FULL_TIME=1
 IS_PART_TIME=2
-NUM_OF_WORKING_DAYS=5
-MAX_HRS_IN_MONTH=20
+NUM_OF_WORKING_DAYS=20
+MAX_HRS_IN_MONTH=50
 
 #variable
 totalEmpHrs=0
-totalworkingDays=0
+totalWorkingDays=0
 
-function getWorkHrs() {
+function getWorkHrs(){
 	local empCheck=$1
 	case $empCheck in
-	$IS_FULL_TIME)
-		empHrs=8
-		;;
-	$IS_PART_TIME)
-		empHrs=4
-		;;
-	*)
-		empHrs=0
-		;;
-	esac
-	echo $empHrs
+		$IS_FULL_TIME)
+			empHrs=8
+			;;
+		$IS_PART_TIME)
+			empHrs=4
+			;;
+		*)
+			empHrs=0
+			;;
+		esac
+		echo $empHrs
 }
-
-while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH && $totalworkingDays -lt $NUM_OF_WORKING_DAYS ]]  
+function calDailywage() {
+	local workHrs=$1
+	wage=$(($empHrs*$EMP_RATE_PER_HR))
+	echo $wage
+}
+while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_OF_WORKING_DAYS ]]
 do
-	((totalworkingDays+1))
-	empCheck=$(( RANDOM%3 ))
+	((totalWorkingDays++))
+	empCheck=$((RANDOM%3))
 	empHrs="$( getWorkHrs $empCheck )"
 	totalEmpHrs=$(($totalEmpHrs+$empHrs))
+	dailyWage[$totalWorkingDays]=$(calDailywage $empHrs)
 done
-
-totalSalary=$(($totalEmpHrs*$EMP_RATE_PER_HR))
+echo totalWage=$(($totalEmpHrs*$EMP_RATE_PER_HR))
+echo "Daily Wage:"${dailyWage[@]}
+echo "Day : "${!dailyWage[@]}
 
